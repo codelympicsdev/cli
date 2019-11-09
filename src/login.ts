ef1eb7e4bfc9c80171e603978d2e12d92b2849f0
Luca Casonato <lucacasonato@yahoo.com>
2019-11-09 02:42:45 +0100

    Updated to use oauth2

--- src/login.ts
+++ src/login.ts
@@ -4,36 +4,43 @@ import { config } from './util/config';
 import express from 'express';
 // @ts-ignore
 import openurl from 'openurl';
+import ClientOAuth2 from 'client-oauth2';
 
-const loginPage =
-  'https://auth.codelympics.dev/auth?client_id=5d23b4d2866d0626232bed81';
+var auth = new ClientOAuth2({
+  clientId: '5d23b4d2866d0626232bed81',
+  accessTokenUri: 'https://auth.codelympics.dev/oauth2/token',
+  authorizationUri: 'https://auth.codelympics.dev/oauth2/auth',
+  redirectUri: 'http://localhost:5555/auth_return',
+  scopes: [['user.basic', 'challenge.attempt.write'].join(',')],
+});
+
+const loginPage = auth.token.getUri();
 
 export default async function login(cmd: { ci: boolean }) {
   const spinner = ora('Starting callback server').start();
-
   const app = express();
 
-  app.get('/auth_return', (req, res) => {
+  app.get('/auth_return', async (req, res) => {
+    const token = req.query.access_token;
+    if (!token) {
+      res.send(
+        '<script>location.search = location.hash.replace("#", "")</script>'
+      );
+      return;
+    }
+
     res.send('You can now close this page and return to the CLI.');
     server.close();
 
-    const token = req.query['token'];
-    if (token) {
-      const decoded = decode(token);
+    const decoded = decode(token);
 
-      console.log(`Hello ${(decoded as { full_name: string }).full_name}`);
-
-      if (cmd.ci) {
-        spinner.succeed(`Recieved token:`);
-        console.log(token);
-        return;
-      }
-
-      config.set('token', token);
-
-      spinner.succeed(`Saved token`);
+    if (cmd.ci) {
+      spinner.succeed(`Recieved token:`);
+      console.log(token);
+      return;
     } else {
-      spinner.fail(`Did not recieve a token.`);
+      config.set('token', token);
+      spinner.succeed(`Hello ${(decoded as { full_name: string }).full_name}`);
     }
   });
 
